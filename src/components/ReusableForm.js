e996161d8f53d7c766a86b3f4b64c9f4501717e0
Kevin Davis <thekidnamedkd@gmail.com>
2020-09-23 10:54:50 -0700

    update values for reusableform and set to firestore
    Co-authored-by: Sara Kane <kane.sarak@gmail.com>
    Co-authored-by: Brittany Lindgren <brittdevelopsit@gmail.com>

--- src/components/ReusableForm.js
+++ src/components/ReusableForm.js
@@ -9,50 +9,50 @@ function ReusableForm(props) {
           type='text'
           name='q1'
           placeholder='Enter your first question' />
-        <input
-          type='text'
-          name='q1response1'
-          placeholder='First answer' />
-        <input
-          type='text'
-          name='q1response2'
-          placeholder='Second answer' />
-        <input
-          type='text'
-          name='q1response3'
-          placeholder='Third answer' />
+          <input
+            type='text'
+            name='q1a1'
+            placeholder='First answer' />
+          <input
+            type='text'
+            name='q1a2'
+            placeholder='Second answer' />
+          <input
+            type='text'
+            name='q1a3'
+            placeholder='Third answer' />
         <input
           type='q2'
           name='location'
           placeholder='Enter your second question' />
-        <input
-          type='text'
-          name='q2response1'
-          placeholder='First answer' />
-        <input
-          type='text'
-          name='q2response2'
-          placeholder='Second answer' />
-        <input
-          type='text'
-          name='q2response3'
-          placeholder='Third answer' />
+          <input
+            type='text'
+            name='q2a1'
+            placeholder='First answer' />
+          <input
+            type='text'
+            name='q2a2'
+            placeholder='Second answer' />
+          <input
+            type='text'
+            name='q2a3'
+            placeholder='Third answer' />
         <input
           type='text'
           name='q3'
           placeholder='Enter your third question' />
-        <input
-          type='text'
-          name='q3response1'
-          placeholder='First answer' />
-        <input
-          type='text'
-          name='q3response2'
-          placeholder='Second answer' />
-        <input
-          type='text'
-          name='q3response3'
-          placeholder='Third answer' />
+          <input
+            type='text'
+            name='q3a1'
+            placeholder='First answer' />
+          <input
+            type='text'
+            name='q3a2'
+            placeholder='Second answer' />
+          <input
+            type='text'
+            name='q3a3'
+            placeholder='Third answer' />
 
         <button type='submit'>{props.buttonText}</button>
       </form>
